e2cebc3b710c6066d574dc9a297e7b7a764ccd66
hmnshgpt455 <hmnshgpt455@gmail.com>
2020-11-21 13:47:34 +0530

    Removed unused components

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyD0qSHlC2MoOYphX9mQL4qPRzD4kmNYPrg';
 const BASE_URL = 'https://www.googleapis.com/youtube/v3/';
 
 export default axios.create({ baseURL: BASE_URL });
